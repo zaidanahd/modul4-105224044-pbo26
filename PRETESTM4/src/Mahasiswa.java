f17552e819ccd59021a1eb5d85fe2017ce0f2f9d
zaidanahd <ahmadzaidan0512@gmail.com>
2026-03-31 14:42:43 +0700

    pretestM4

---
+++ PRETESTM4/src/Mahasiswa.java
@@ -0,0 +1,12 @@
+public class Mahasiswa {
+    String nama;
+    int umur;
+    String jurusan;
+
+    public Mahasiswa(String nama, int umur, String jurusan) {
+        this.nama = nama;
+        this.umur = umur;
+        this.jurusan = jurusan;
+    
+    }
+}
